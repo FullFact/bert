9f33d83801d3330608b926771668ff5190401fbc
David Corney <david.corney@fullfact.org>
2019-07-19 10:26:03 +0100

    Get checkpoint id automatically for tests

--- test_bert.sh
+++ test_bert.sh
@@ -1,14 +1,18 @@
+#!/bin/bash
 # ::::::::::::::
 # test_bert.sh
 # ::::::::::::::
 
 # Use a trained model to classify test data
+# First, get id of trained classifier model from checkpoint file.
+MODEL=$(sed -n 's/^model_checkpoint_path.*"\(.*\)"/\1/p' bert_output/checkpoint)
 
-# Need to specify trained classifier to be used based on last (=best) model created during training
-export TRAINED_CLASSIFIER=./bert_output/model.ckpt-112
+export TRAINED_CLASSIFIER="./bert_output/$MODEL"
 export BERT_BASE_DIR=./multi_cased_L-12_H-768_A-12
 
-python run_classifier.py \
+echo "Using model: $TRAINED_CLASSIFIER  on top of BERT base $BERT_BASE_DIR"
+
+python run_multilabels_classifier.py \
 --task_name=multilabel \
 --do_predict=true \
 --data_dir=./data \
